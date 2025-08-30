70eda959d3b73d154ec109c47aaea8d08ffa682f
HMcVeigh <hughmcv@gmail.com>
2025-08-30 16:48:40 +0100

    Added functionality for getting club and course id's in record round

--- rounds/roundManagement.js
+++ rounds/roundManagement.js
@@ -6,10 +6,14 @@ export function recordRound(){
     console.log("\n--- Record New Round ---\n")
     rl.question("Enter Golf Club: ", (club) =>{
         // Check if entered club exists
-        db.get(`SELECT COUNT(*) as count FROM clubs WHERE name = ?`, [club], (err, row)=>{
-            if (err || row.count == 0){
-                console.log("Entered club does not exist - have admin register club and try again");
-                showMenu();
+        db.get(`SELECT COUNT(*) as count FROM clubs WHERE name = ?`, [club.trim()], (err, row)=>{
+            if (err){
+                console.error("Error Getting Club: ", err.message);
+                return showMenu();
+            }
+            else if (row.count == 0){
+                console.log("Club not found - try again or add new club in admin menu");
+                return recordRound();
             }
             else{
                 getCourse(club);
@@ -21,6 +25,28 @@ export function recordRound(){
 function getCourse(club){
     rl.question(`Enter Course at ${club}: `, (course)=>{
         // Check that given course at location exists
-        ;
+        db.get(`SELECT id FROM clubs WHERE name = ?`, [club.trim()], (err, row)=>{
+            if (err){
+                console.error("Error Getting ClubID: ", err.message);
+                return showMenu();
+            }
+            else if (!row){
+                console.log("Course not found - try again or add new course in admin menu");
+                return recordRound();
+            }
+            else{
+                let clubID = row.id;
+                // Get course id where name and club id match
+                db.get(`SELECT id FROM courses WHERE name = ? AND club_id = ?`, [course.trim(), clubID], (err, row)=>{
+                    if (err){
+                        console.error("Error Getting CourseID: ", err.message);
+                        return showMenu();
+                    }
+                    else{
+                        let courseID = row.id;
+                    }
+                })
+            }
+        });
     })
 }
