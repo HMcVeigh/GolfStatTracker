759095f25e21fc3616f124122231ff3f7b11b06b
HMcVeigh <hughmcv@gmail.com>
2025-08-31 14:19:17 +0100

    Added save new round capabilities

--- rounds/roundManagement.js
+++ rounds/roundManagement.js
@@ -31,22 +31,60 @@ function getCourse(club){
                 return showMenu();
             }
             else if (!row){
-                console.log("Course not found - try again or add new course in admin menu");
+                console.log("Club not found");
                 return recordRound();
             }
             else{
                 let clubID = row.id;
                 // Get course id where name and club id match
-                db.get(`SELECT id FROM courses WHERE name = ? AND club_id = ?`, [course.trim(), clubID], (err, row)=>{
+                db.get(`SELECT id FROM courses WHERE course_name = ? AND club_id = ?`, [course.trim(), clubID], (err, row)=>{
                     if (err){
                         console.error("Error Getting CourseID: ", err.message);
                         return showMenu();
                     }
+                    else if(!row){
+                        console.log("Course not found - change spelling or add course in admin menu");
+                        return recordRound();
+                    }
                     else{
                         let courseID = row.id;
+                        getScore(clubID, courseID)
                     }
                 })
             }
         });
     })
+}
+
+function getScore(clubID, courseID){
+    // Get score and do some verification
+    rl.question("Enter your numeric score after 18 holes (e.g. 75): ", (score) =>{
+        if (!score || score.trim() == ''){
+            console.log("Please enter a score\n");
+            return getScore(clubID, courseID);
+        }
+        const numScore = parseInt(score.trim());
+        if (!isNaN(numScore)){
+            saveRound(clubID, courseID, numScore);
+        }
+        else{
+            console.log("Enter a valid number between 50 and 160\n");
+            return getScore(clubID, courseID);
+        }
+    })
+}
+
+function saveRound(clubID, courseID, numScore){
+    const tempPlayerID = 1;
+    db.run(`INSERT INTO rounds (club_id, course_id, player_id, score) VALUES (?,?,?,?)`,
+        [clubID, courseID, tempPlayerID, numScore], (err)=>{
+            if (err){
+                console.error("Error adding round: ", err.message);
+            }
+            else{
+                console.log(`Successfully added round`);
+            }
+            showMenu();
+        }
+    )
 }
